bee20d8adf23059715e7200aee553b2ea33e6919
THELASTOFUS <PRINCETONKING@GMAIL.COM>
2021-05-09 02:40:59 -0700

    Update radarr-install.sh

--- radarr/radarr-install.sh
+++ radarr/radarr-install.sh
@@ -30,8 +30,8 @@ cd $installdir || exit
 
 echo "Installing radarr. Please wait!"
 wget -q "$( wget -qO- https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )"
-tar -xzf Radarr.develop.*.linux.tar.gz
-rm -f Radarr.develop.*.linux.tar.gz
+tar -xzf Radarr.master.*.linux.tar.gz
+rm -f Radarr.master.*.linux.tar.gz
 chown -R $user:$user Radarr
 
 # Create startup service
