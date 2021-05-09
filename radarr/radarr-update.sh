e2033e946a2b8dbc35fc01bdd6c0e113dc6dacd6
THELASTOFUS <PRINCETONKING@GMAIL.COM>
2021-05-09 03:00:09 -0700

    Update radarr-update.sh

--- radarr/radarr-update.sh
+++ radarr/radarr-update.sh
@@ -13,8 +13,8 @@ service radarr stop
 
 echo "Updating radarr. Please wait!"
 wget -q "$( wget -qO- https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )"
-tar -xzf Radarr.develop.*.linux.tar.gz
-rm -f Radarr.develop.*.linux.tar.gz
+tar -xzf Radarr.master.*.linux.tar.gz
+rm -f Radarr.master.*.linux.tar.gz
 chown -R $user:$user Radarr
 
 # start radarr now
